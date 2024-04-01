cd8abe59675000328b956ca60b12911ca3c9679e
Bhaskar Boruah <boruah.bhaskar@gmail.com>
2024-03-31 23:00:09 -0500

    updated main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-#from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
