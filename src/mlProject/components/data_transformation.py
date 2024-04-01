41a4b3eaaf9576b6fd6b7fcabd330f14586ab427
Bhaskar Boruah <boruah.bhaskar@gmail.com>
2024-03-31 23:51:38 -0500

    updated data transformation

--- src/mlProject/components/data_transformation.py
+++ src/mlProject/components/data_transformation.py
@@ -20,7 +20,7 @@ class DataTransformation:
     def train_test_spliting(self):
         data = pd.read_csv(self.config.data_path)
         
-        data = data.drop(['type'], axis=1)
+        #data = data.drop(['type'], axis=1)
 
         # Split the data into training and test sets. (0.75, 0.25) split.
         train, test = train_test_split(data)
