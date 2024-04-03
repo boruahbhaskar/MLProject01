5e9bec28985b6e7b16b138725a848b74c2c9af7e
Bhaskar Boruah <boruah.bhaskar@gmail.com>
2024-04-02 21:54:11 -0500

    updated prediction.py file

--- src/mlProject/pipeline/prediction.py
+++ src/mlProject/pipeline/prediction.py
@@ -1,15 +1,16 @@
-import joblib
+import joblib 
 import numpy as np
 import pandas as pd
-import pathlib as Path
+from pathlib import Path
+
 
 
 class PredictionPipeline:
-    def __init__(self) -> None:
+    def __init__(self):
         self.model = joblib.load(Path('artifacts/model_trainer/model.joblib'))
-        
+
+    
     def predict(self, data):
         prediction = self.model.predict(data)
-        
-        return prediction    
-        
+
+        return prediction
